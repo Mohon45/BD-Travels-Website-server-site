fdd2cec3e3ff5e36c940dc186e597b78c2bf7a36
Md: Mohon <mdmohon6145@gmail.com>
2022-08-03 15:48:08 +0600

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
 
 const port = process.env.PORT || 5000;
@@ -11,52 +11,50 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0coh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-async function run () {
-    try{
-        await client.connect();
-        const database = client.db('our_service');
-        const serviceCollection = database.collection('services');
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("our_service");
+    const serviceCollection = database.collection("services");
 
-        // GET Services API
-        app.get('/services', async(req, res) => {
-            const cursor = serviceCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
-        });
+    // GET Services API
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        // Add Services
-        app.post('/addServices', async (req, res) => {
-            const addService = req.body;
-            const result = await serviceCollection.insertOne(addService);
-            res.json(result);
-        });
+    // Add Services
+    app.post("/addServices", async (req, res) => {
+      const addService = req.body;
+      const result = await serviceCollection.insertOne(addService);
+      res.json(result);
+    });
 
-        // get single product
-        app.get("/singleProduct/:id", async (req, res) => {
-            const result = await servicesCollection
-            .find({ _id: ObjectId(req.params.id) })
-            .toArray();
-            res.send(result[0]);
-        });
-    }
-    finally{
-        // await client.close();
-    }
+    // get single product
+    app.get("/singleProduct/:id", async (req, res) => {
+      const result = await servicesCollection
+        .find({ _id: ObjectId(req.params.id) })
+        .toArray();
+      res.send(result[0]);
+    });
+  } finally {
+    // await client.close();
+  }
 }
-
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Traveling-Sarver Running');
-})
-
+app.get("/", (req, res) => {
+  res.send("Traveling-Sarver Running");
+});
 
 app.listen(port, () => {
-    console.log('Traveling-Sarver Running', port)
-})
+  console.log("Traveling-Sarver Running", port);
+});
